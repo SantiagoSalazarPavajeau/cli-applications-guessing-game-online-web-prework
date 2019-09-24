e97bbf297826367163def01420b01a9a89210bbe
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-09-24 22:58:52 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elseif input != number.to_s
     puts "Sorry! The computer guessed #{number}."
-  else input == exit!
+  else input.downcase == "exit"
     puts "Goodbye!"
   end
 end
